06704ee63d9394984c02b3d02ea6a1621c9c5f30
TheMith <77514217+TheMithh@users.noreply.github.com>
2025-04-03 11:32:21 -0400

    Changed my frotend components again, since it doesnt work as its supposed to

--- frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { EmployeeManagementComponent } from './employee-management/employee-management.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'employees', component: EmployeeManagementComponent },
-];
